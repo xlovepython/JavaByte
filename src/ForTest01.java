731887769e6ce194ac46ae866faf2598b930b91c
xlovepython <xlovepython@gmail.com>
2020-05-16 18:58:39 +0800

    continue  当运行到这里时  循环下面的代码不执行了 ，继续开始下一次循环

--- src/ForTest01.java
+++ src/ForTest01.java
@@ -4,7 +4,7 @@ public class ForTest01 {
     public static void main(String[] args) {
         for(int i = 1; i < 10; i++){
             for(int j = 1; j<=i; j++){
-                System.out.print(i + "*" + j+ "=" + i*j + " ");
+                System.out.print(j + "*" + i+ "=" + i*j + " ");
             }
             System.out.println();
         }
